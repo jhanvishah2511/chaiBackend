c6863972fe6506b7f36cfabf888a7c707f169cfa
jhanvi.shah <jhanvi.shah@iflair.com>
2024-02-08 19:35:14 +0530

    utils files added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import dotenv from "dotenv";
 import { mongoose } from "mongoose";
 import connectDb from "./db/index.js";
+import app from "./app.js"
 
 dotenv.config({
   path: "./env",
 });
 
-connectDb();
+connectDb()
+.then(()=>{
+    app.listen(process.env.PORT|| 8000,()=>{
+        console.log(`App is running on port${process.env.PORT}`)
+    })
+})
+.catch((Err)=>{
+    console.log('Mongodb connection failed', Err)
+})
 
 // async function connectDb() {
 //   try {
